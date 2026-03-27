8f3a50e7a011030e3edd8e30067120dcd0271cad
anupam1708 <anupam.1708@gmail.com>
2026-03-27 13:48:47 -0500

    fix: Upgrade Java 11 → 21 (LTS): applied OpenRewrite recipes (org.openrewrite.java.migrate.UpgradeToJava2

--- src/main/java/com/pluralsight/hibernatefundamentals/airport/Passenger.java
+++ src/main/java/com/pluralsight/hibernatefundamentals/airport/Passenger.java
@@ -1,6 +1,6 @@
 package com.pluralsight.hibernatefundamentals.airport;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
